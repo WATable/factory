9493ad15f460ef1ea95cf8327f33a1ab0a159c05
snake <snake@cosyjoy.com>
2018-11-15 19:38:19 +0800

    Main change

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,7 +8,14 @@ int main(int argc, char const *argv[])
     CreateFactory *cf = new CreateFactory();
 
     Object * ob = cf->Create(0);
-    printf("HelloWorld\n%s",(ob->ToString().c_str()));
+    printf("HelloWorld\n%s\n",(ob->ToString().c_str()));
+
+    char str  = 'p';
+
+    int ret = (int)str;
+
+    printf("%d",ret);
+	printf("%s", ret);
     return 0;
 }
 
